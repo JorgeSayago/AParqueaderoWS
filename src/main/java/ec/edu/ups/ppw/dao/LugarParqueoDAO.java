b2d3161c71e88399f283b4bc5b072620fe6d89f2
juanfranqf2002 <juanfranciscoqf@gmail.com>
2023-07-26 23:33:20 -0500

    CreacionLugarParqueoDAO

--- src/main/java/ec/edu/ups/ppw/dao/LugarParqueoDAO.java
+++ src/main/java/ec/edu/ups/ppw/dao/LugarParqueoDAO.java
@@ -1,5 +1,43 @@
 package ec.edu.ups.ppw.dao;
 
-public class LugarParqueoDAO {
+import java.io.Serializable;
+import java.util.List;
 
+import ec.edu.ups.ppw.modelo.Carro;
+import ec.edu.ups.ppw.modelo.LugarParqueo;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+
+@Stateless
+public class LugarParqueoDAO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@PersistenceContext
+	private EntityManager em;
+	
+	public void insert(LugarParqueo lugarParqueo) {
+		em.persist(lugarParqueo);
+	}
+	
+	public void update(LugarParqueo lugarParqueo) {
+		em.merge(lugarParqueo);
+	}
+	
+	public LugarParqueo read(int codigo) {
+		LugarParqueo l = em.find(LugarParqueo.class, codigo);
+		return l;
+	}
+	
+	public void delete(int codigo) {
+		LugarParqueo l = em.find(LugarParqueo.class, codigo);
+		em.remove(l);
+	}
+	
+	public List<LugarParqueo> getAll(){
+		String jpql = "SELECT l FROM LugarParqueo l";
+		Query q = em.createQuery(jpql);
+		return q.getResultList();
+	}
 }
