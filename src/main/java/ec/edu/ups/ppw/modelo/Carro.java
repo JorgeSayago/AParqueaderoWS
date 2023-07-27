6b5908b7da3dd3a45a429c6dc8c54d94f0d38b1b
juanfranqf2002 <juanfranciscoqf@gmail.com>
2023-07-26 23:04:44 -0500

    Update Carro.java

--- src/main/java/ec/edu/ups/ppw/modelo/Carro.java
+++ src/main/java/ec/edu/ups/ppw/modelo/Carro.java
@@ -1,5 +1,21 @@
 package ec.edu.ups.ppw.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class Carro {
 
+	@Id
+	@Column(name="car_placa")
+	private String placa;
+	
+	
 }
