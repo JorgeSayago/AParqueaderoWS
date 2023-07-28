ff7fbf57a4392b6b295d26130020d7081f819aca
JorgeSayago <130063995+JorgeSayago@users.noreply.github.com>
2023-07-27 21:56:24 -0500

    GestionPersona
    implementacion de metodos en la clase GestionPersona

--- src/main/java/ec/edu/ups/ppw/negocio/GestionPersona.java
+++ src/main/java/ec/edu/ups/ppw/negocio/GestionPersona.java
@@ -1,5 +1,81 @@
 package ec.edu.ups.ppw.negocio;
 
-public class GestionPersona {
+import java.util.List;
 
+
+import ec.edu.ups.ppw.dao.PersonaDAO;
+import ec.edu.ups.ppw.modelo.Persona;
+import jakarta.ejb.Stateless;
+import jakarta.inject.Inject;
+
+@Stateless
+public class GestionPersona {
+	@Inject
+	private PersonaDAO daoPersona; 
+	
+	public void guardarClientes(Persona persona) throws Exception {
+		if(!this.isCedulaValida(persona.getCedula()))
+			throw new Exception("Cedula incorrecta");
+		
+		if(daoPersona.read(persona.getCedula()) == null) {
+			try {
+				daoPersona.insert(persona);
+			}catch(Exception e) {
+				throw new Exception("Error al insertar: " + e.getMessage());
+			}
+		}else {
+			try {
+				daoPersona.update(persona);
+			}catch(Exception e) {
+				throw new Exception("Error al actualizar: " + e.getMessage());
+			}
+		}
+	}
+	
+	private boolean isCedulaValida(String cedula) {
+		return cedula.length() == 10;
+	}
+public void guardarClientes(String cedula, String nombre, String direccion) {
+		
+	}
+	
+	public void eliminarCliente(Persona persona) throws Exception {
+	    if (!this.isCedulaValida(persona.getCedula())) {
+	        throw new Exception("Cedula incorrecta");
+	    }
+
+	    Persona persona1 = daoPersona.read(persona.getCedula());
+	    if (persona1 == null) {
+	        throw new Exception("El cliente con la cedula " + persona.getCedula() + " no existe");
+	    }
+
+	    try {
+	        daoPersona.delete(persona.getCedula());
+	    } catch (Exception e) {
+	        throw new Exception("Error al eliminar: " + e.getMessage());
+	    }
+	}
+	
+	public void actualizarCliente(Persona persona) throws Exception {
+	    if (!isCedulaValida(persona.getCedula())) {
+	        throw new Exception("Cédula incorrecta");
+	    }
+
+	    Persona personaExistente = daoPersona.read(persona.getCedula());
+	    if (personaExistente == null) {
+	        throw new Exception("El cliente con la cédula " + persona.getCedula() + " no existe");
+	    }
+
+	    try {
+	        daoPersona.update(persona); 
+	    } catch (Exception e) {
+	        throw new Exception("Error al actualizar: " + e.getMessage());
+	    }
+	}
+
+	
+	public List<Persona> getClientes(){
+		return daoPersona.getAll();
+	}
+	
 }
